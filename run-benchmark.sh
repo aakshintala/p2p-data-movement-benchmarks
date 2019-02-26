fdb3303834cea146e9351ab59b14f90c21a40d5d
Amogh Akshintala <aakshintala@cs.stonybrook.edu>
2019-02-25 19:47:24 -0600

    minor changes

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -x #echo on
 
 # Make a new directory for each run.
 topdir=$(pwd)
@@ -7,7 +8,7 @@ mkdir $benchmarkDir
 cd $benchmarkDir
 
 let totalDataMoved=12*1000*1000*1000/4; # the benchmark uses ints, so divide by sizeof(int)
-let elemSize=1000; # Because each element is a 32-bit int, that's 4kb (1 page)
+let elemSize=10000; # Because each element is a 32-bit int, that's 40kb (1 page)
 let computeTime=10; # Time in microseconds that simulates 'work' Range 10u to 100m
 while [[ $elemSize -lt 4000000000 ]]; do
 	let numElems=$totalDataMoved/$elemSize;
@@ -15,9 +16,9 @@ while [[ $elemSize -lt 4000000000 ]]; do
 		subDir=$numElems-$elemSize-$computeTime
 		mkdir $subDir
 		cd $subDir
-		sudo ./p2pbenchmark -q $numElems -s $elemSize -t $computeTime > timing
+		sudo $topdir/p2pbenchmark -q $numElems -s $elemSize -t $computeTime > timing
 		cd ..
-		let $computeTime=$computeTime*10;
+		let computeTime=$computeTime*10;
 	done
 	let elemSize=$elemSize*4;
 done
